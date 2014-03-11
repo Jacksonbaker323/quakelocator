c1fe381afb66730049fe4c466a7c953f035032b2
Jacksonbaker323 <jackson@jacksonbaker.net>
2014-03-10 22:52:47 -0700

    Super-duper update
    Updated to a Yeoman generated angular project. Deleted the old files.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,63 @@
+'use strict';
+
+angular.module('quakelocatorApp',['google-maps'])
+  .controller('MainCtrl', ['$scope', 'GeolocationService', function ($scope, geolocation) {
+
+  //Get Location data
+  $scope.get_location = function() {
+    $scope.map = {
+      center: {
+        latitude: 0,
+        longitude: 0
+      },
+      zoom: 8
+    }
+    //Get information from the Geolocation factory service
+    var position = null;
+    geolocation().then(function(position){
+      $scope.map = {
+        center: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+      }
+    }, function(reason){
+      console.log('cant get location');
+    });
+  }
+}]);
+
+
+
+//Helper Functions
+
+function parse_data(data, latitude, longitude) {
+    var quakes = [];
+    data.features.map(function(feature){
+        var quake = {};
+        quake.Magnitude = feature.properties.mag;
+        quake.Longitude = feature.geometry.coordinates[0];
+        quake.Latitude = feature.geometry.coordinates[1];
+        quake.Depth = feature.geometry.coordinates[2];
+        quake.Place = feature.properties.place;
+        quake.Distance = calc_distance(latitude, longitude, quake.Latitude, quake.Longitude);
+        quakes.push(quake);
+    });
+    return quakes;
+}
+
+function calc_distance(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return Math.floor(d);
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI/180);
+}
---
+++ app/scripts/services/geolocationservice.js
@@ -0,0 +1,24 @@
+'use strict';
+//http://proccli.com/2013/10/angularjs-geolocation-service
+angular.module('quakelocatorApp')
+  .factory('GeolocationService', ['$q', '$window','$rootScope', function ($q, $window, $rootScope) {
+    return function() {
+      var d = $q.defer();
+      if (!$window.navigator) {
+        $rootScope.$apply(function(){
+          d.reject(new Error('Geolocation is not supported'));
+        });
+      } else {
+        $window.navigator.geolocation.getCurrentPosition(function(position){
+          $rootScope.$apply(function(){
+            d.resolve(position);
+          });
+        }, function(error) {
+          $rootScope.$apply(function(){
+            d.reject(error);
+          });
+        });
+      }
+      return d.promise;
+    }
+  }]);
